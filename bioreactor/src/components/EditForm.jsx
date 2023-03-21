7030e471e41b5403536a07919acba08de866a125
Frankreed <baoanhtdn@gmail.com>
2023-03-20 21:10:30 -0700

    something something happened lmao

--- bioreactor/src/components/EditForm.jsx
+++ bioreactor/src/components/EditForm.jsx
@@ -1,82 +1,88 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { getRecipe } from "../redux/selectors";
+import React, {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+import {getRecipe} from "../redux/selectors";
 import Ingredients from "./Ingredients";
 
-const EditForm = ({ onClickHandler }) => {
-  const selectedRecipe = useSelector(getRecipe);
-  useEffect(() => {
-    console.log("selected recipe changed:", selectedRecipe);
-  }, [selectedRecipe]);
-  const [name, setName] = useState(selectedRecipe.name);
-  //   const [ingredients, setIngredients] = useState(selectedRecipe.Ingredients);
+const EditForm = ({onClickHandler}) => {
+    const selectedRecipe = useSelector(getRecipe);
+    useEffect(() => {
+        console.log("selected recipe changed:", selectedRecipe);
+    }, [selectedRecipe]);
+    const [name, setName] = useState(selectedRecipe.name);
+    //   const [ingredients, setIngredients] = useState(selectedRecipe.Ingredients);
 
-  const handleChangeName = (e) => {
-    setName(e.target.value);
-  };
+    const handleChangeName = (e) => {
+        setName(e.target.value);
+    };
 
-  return (
-    <>
-      <form className="addform">
-        <div className="form-title">Edit your recipe</div>
-        <label>
-          <div className="subject">
-            Recipe Name<span className="required"> *</span>
-          </div>
-          <input
-            required
-            type="text"
-            name="name"
-            value={name}
-            onInput={handleChangeName}
-            className="general-text-input"
-          />
-        </label>
-        <label>
-          <div className="subject">
-            Required Temperature<span className="required"> *</span>
-          </div>
-          <input
-            required
-            type="text"
-            name="temperature"
-            className="general-text-input"
-            value={selectedRecipe.temperature}
-          />
-        </label>
-        <label>
-          <div className="subject">
-            Required Motor RPM<span className="required"> *</span>
-          </div>
-          <input
-            required
-            type="text"
-            name="rpm"
-            className="general-text-input"
-            value={selectedRecipe.motorSpeed}
-          />
-        </label>
-        {selectedRecipe && (
-          <label>
-            <div className="subject">
-              Required Ingredient<span className="required"> *</span>
-            </div>
-            <Ingredients recipe={selectedRecipe.Ingredients} />
-          </label>
-        )}
-        <div>
-          <input type="submit" value="Edit" className="form-button submit" />
-          <button
-            type="button"
-            onClick={onClickHandler}
-            className="form-button cancel"
-          >
-            Cancel
-          </button>
-        </div>
-      </form>
-    </>
-  );
+    let recipeLabel = null
+
+    if (selectedRecipe) {
+        console.log("INGREDIENTS:", selectedRecipe)
+        recipeLabel =
+            <label>
+                <div className="subject">
+                    Required Ingredient<span className="required"> *</span>
+                </div>
+                <Ingredients recipe={selectedRecipe.ingredients}/>
+            </label>
+    }
+
+    return (
+        <>
+            <form className="addform">
+                <div className="form-title">Edit your recipe</div>
+                <label>
+                    <div className="subject">
+                        Recipe Name<span className="required"> *</span>
+                    </div>
+                    <input
+                        required
+                        type="text"
+                        name="name"
+                        value={name}
+                        onInput={handleChangeName}
+                        className="general-text-input"
+                    />
+                </label>
+                <label>
+                    <div className="subject">
+                        Required Temperature<span className="required"> *</span>
+                    </div>
+                    <input
+                        required
+                        type="text"
+                        name="temperature"
+                        className="general-text-input"
+                        value={selectedRecipe.temperature}
+                    />
+                </label>
+                <label>
+                    <div className="subject">
+                        Required Motor RPM<span className="required"> *</span>
+                    </div>
+                    <input
+                        required
+                        type="text"
+                        name="rpm"
+                        className="general-text-input"
+                        value={selectedRecipe.motorSpeed}
+                    />
+                </label>
+                {recipeLabel}
+                <div>
+                    <input type="submit" value="Edit" className="form-button submit"/>
+                    <button
+                        type="button"
+                        onClick={onClickHandler}
+                        className="form-button cancel"
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </>
+    );
 };
 
 export default EditForm;
